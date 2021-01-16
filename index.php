df131d1933c3117565852f23ade21d2205f2c96f
unknown <jonatta89@gmail.com>
2021-01-16 14:00:22 -0300

    task

--- index.php
+++ index.php
@@ -1,2 +1,16 @@
-<?php
-
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>task</title>
+</head>
+<body>
+  <form action="">
+    <input type="text" name="login"><br>
+    <input type="password" name="senha"><br>
+    <input type="email" name="email"><br>
+    <input type="submit" value="Enviar"> 
+  </form>
+</body>
+</html>
