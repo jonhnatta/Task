a9ca911b83a561d26d472f1726518031a0143a73
jonhnatta Santos <jonatta89@gmail.com>
2021-01-20 17:20:20 -0300

    delete viwes

--- index.php
+++ index.php
@@ -20,7 +20,7 @@ if (isset($_GET['erro'])) {
 <head>
   <meta charset="UTF-8">
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
-  <title>task</title>
+  <title>Task</title>
   <link rel="stylesheet" href="css/style.css">
   <link href="//maxcdn.bootstrapcdn.com/bootstrap/4.1.1/css/bootstrap.min.css" rel="stylesheet" id="bootstrap-css">
   <script src="//maxcdn.bootstrapcdn.com/bootstrap/4.1.1/js/bootstrap.min.js"></script>
